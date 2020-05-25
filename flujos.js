7341d20e90b2e8cd5627f802237d44aeb009a46e
Chucho <alarconcito_jr@hotmail.com>
2020-05-25 15:51:40 -0400

    actualizamos la clase flujos escribiendo el codigo para trabajr con buffer vacios

--- flujos.js
+++ flujos.js
@@ -1,9 +1,13 @@
+//const fs = require('fs')
 class flujos{
     ejemploBuffer(){
-
+        const buffer_vacio = Buffer.alloc(20);
+        console.log(buffer_vacio);
     }
 
     ejemploStream(){
         
     }
 }
+const f = new flujos();
+f.ejemploBuffer()
