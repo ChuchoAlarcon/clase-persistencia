81f8d15dd8062658123b2c02abce0238e9025757
Chucho <alarconcito_jr@hotmail.com>
2020-05-26 16:21:22 -0400

    actualizaciond e stream

--- flujos.js
+++ flujos.js
@@ -1,13 +1,39 @@
-//const fs = require('fs')
-class flujos{
-    ejemploBuffer(){
+const fs = require('fs')
+class flujos {
+    ejemploBuffer() {
         const buffer_vacio = Buffer.alloc(20);
         console.log(buffer_vacio);
+        const buffer_cadena = Buffer.from('@emi@ @riberalta');
+        console.log(buffer_cadena);
     }
 
-    ejemploStream(){
-        
+    ejemploStream() {
+
+    }
+
+    ejemploReadFile(nombre, tipo) {
+        /*
+        const objeto = {
+            nombre: 'juan',
+            edad: 45,
+            sexo: true
+        }
+        */
+        const ruta = 'texto.txt'
+        console.log('iniciando')
+
+        fs.readFile(ruta, (err, data) => {
+            console.log(data.toString());
+        });
+
+        console.log('finalizando')
+
+        fs.readFileSync()
+    }
+
+    leerArchivo = (nombre, tipo) => {
+
     }
 }
 const f = new flujos();
-f.ejemploBuffer()
+f.ejemploReadFile()
